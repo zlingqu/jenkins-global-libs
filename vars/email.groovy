e4a02f75efbd81b248e8fc89b0048c1414e81316
左胜 <zuosheng@dm-ai.cn>
2019-05-21 04:42:42 -0700

    test

--- vars/email.groovy
+++ vars/email.groovy
@@ -6,35 +6,24 @@ def call() {
 }
 
 def successBody(jobName) {
-//    return """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-//<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
     return """Job build successful. Address : http://jenkins.ops.dm-ai.cn/blue/organizations/jenkins/""" + jobName + """/detail/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/pipeline"""
 }
 
 def faildBody(jobName) {
-//    return """
-////<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-////<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
-////"""
     return """Job build faild. Address : Address : http://jenkins.ops.dm-ai.cn/blue/organizations/jenkins/""" + jobName + """/detail/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/pipeline"""
 }
-//
-//def getBuildAddress() {
-//    return """http://jenkins.ops.dm-ai.cn/blue/organizations/jenkins/${env.JOB_NAME}}/detail/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/pipeline"""
-//}
 
-def showEnv(env, String buildResult) {
-    def text = """Job build ${buildResult} Address : http://jenkins.ops.dm-ai.cn/blue/organizations/jenkins/${jobName}/detail/${branchName}/${buildNumber}/pipeline"""
-    def template = new groovy.text.StreamingTemplateEngine().createTemplate(text)
+def showEnv(env, buildResult) {
+    def text = 'Job build $buildResult Address : http://jenkins.ops.dm-ai.cn/blue/organizations/jenkins/$jobName/detail/$branchName/$buildNumber/pipeline'
     def binding = [
-//            'jobName' : env.JOB_NAME.split("/")[0],
             'jobName' : '11111',
             'branchName' : env.BRANCH_NAME,
             'buildNumber' : env.BUILD_NUMBER,
             'buildResult': buildResult,
     ]
-    String response = template.make(binding)
-    return response
+    def engine = new groovy.text.SimpleTemplateEngine()
+    def template = engine.createTemplate(text).make(binding)
+    println(template)
 }
 
 
