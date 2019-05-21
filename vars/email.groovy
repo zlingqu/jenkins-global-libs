957b2f818efe4164f2dd6d980b15d0308b4303f2
左胜 <zuosheng@dm-ai.cn>
2019-05-21 02:52:57 -0700

    test

--- vars/email.groovy
+++ vars/email.groovy
@@ -3,14 +3,19 @@ def call() {
 }
 
 def successBody() {
-    return """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
-"""
+//    return """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+//<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
+    return 'Job build successful. Address : ' + getBuildAddress()
 }
 
 def faildBody() {
-    return """
-<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
-"""
+//    return """
+////<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+////<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>
+////"""
+    return 'Job build faild. Address : ' + getBuildAddress()
+}
+
+def getBuildAddress() {
+    return 'http://jenkins.ops.dm-ai.cn/blue/organizations/jenkins/${env.JOB_NAME}}/detail/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/pipeline'
 }
