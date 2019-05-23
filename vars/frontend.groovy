509bdf2cb112dfab2cc7140089688c449045a261
左胜 <zuosheng@dm-ai.cn>
2019-05-23 00:06:09 -0700

    test

---
+++ vars/frontend.groovy
@@ -0,0 +1,43 @@
+def baseTemplateName() {
+    return 'base-template'
+}
+
+def yarnTemplate() {
+    return """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: yarnTemplate
+  namespace: devops
+spec:
+  imagePullSecrets:
+  - name: regsecret
+  containers:
+  - name: docker-compose
+    image: docker.dm-ai.cn/devops/base-image-docker-compose:0.04
+    imagePullPolicy: IfNotPresent
+    env: #指定容器中的环境变量
+    - name: DMAI_PRIVATE_DOCKER_REGISTRY
+      value: docker.dm-ai.cn  
+    volumeMounts:
+    - name: sock
+      mountPath: /var/run/docker.sock
+    command:
+    - "/bin/sh"
+    - "-c"
+    args:
+    - "cat"
+    tty: true
+    resources:
+      limits:
+        memory: 500Mi
+        cpu: 300m
+      requests:
+        cpu: 100m
+        memory: 200Mi
+  volumes:
+  - name: sock
+    hostPath:
+      path: /var/run/docker.sock      
+"""
+}
