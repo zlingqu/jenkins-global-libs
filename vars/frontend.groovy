53ea3f05060e203d6cb6e62347e61b0ecb799db4
左胜 <zuosheng@dm-ai.cn>
2019-05-23 16:51:21 -0700

    test

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -2,94 +2,94 @@ import java.io.File
 
 def call(Map map, env) {
     println(env)
-    pipeline {
-        agent {
-            kubernetes {
-                cloud 'kubernetes-test'
-                label 'yarnTemplate'
-                defaultContainer 'jnlp'
-                namespace 'devops'
-                inheritFrom baseTemplateName()
-                yaml yarnTemplate()
-            }
-        }
-
-        // 设置整个pipeline 的超时时间为 1个小时
-
-        options {
-            timeout(time:1, unit: 'HOURS')
-            retry(2)
-        }
-
-        // 添加环境变量
-        environment {
-            tags = map.imageTags
-        }
-
-        stages {
-            stage('Compile') {
-                steps {
-                    container('yarn-compile') {
-                        sh '''
-                    chmod -R 777 `pwd`
-                    npm config set registry=http://192.168.3.13:8081/repository/npm/
-                    npm install
-                    npm config set registry=http://192.168.3.13:8081/repository/npm/
-                    npm run build
-                    '''
-                    }
-                }
-            }
-
-            stage('Make image') {
-                steps {
-                    container('docker-compose') {
-                        sh 'docker-compose build'
-                        sh 'docker-compose push'
-                    }
-                }
-            }
-
-            stage('Deploy') {
-                steps {
-                    kubernetesDeploy configs: 'Deploy-k8s.yml', kubeConfig: [path: ''], kubeconfigId: 'k8s-deploy-test', secretName: '', ssh: [sshCredentialsId: '*', sshServer: ''], textCredentials: [certificateAuthorityData: '', clientCertificateData: '', clientKeyData: '', serverUrl: 'https://']
-
-                }
-            }
-
-        }
-
-        post {
-            always {
-                echo "over!!"
-            }
-
-            failure {
-                echo "fail"
-                script {
-                    def jobName = env.JOB_NAME.split("/")[0]
-                    echo jobName
-                    emailext (
-                            body: faildBody(jobName),
-                            subject: 'Jenkins build faild info',
-                            to: 'zuosheng@dm-ai.cn'
-                    )
-                }
-            }
-
-            success {
-                script {
-                    def jobName = env.JOB_NAME.split("/")[0]
-                    echo jobName
-                    emailext (
-                            body: showEnv(env, 'success'),
-                            subject: 'Jenkins build success info',
-                            to: 'qinyadong@dm-ai.cn'
-                    )
-                }
-            }
-        }
-    }
+//    pipeline {
+//        agent {
+//            kubernetes {
+//                cloud 'kubernetes-test'
+//                label 'yarnTemplate'
+//                defaultContainer 'jnlp'
+//                namespace 'devops'
+//                inheritFrom baseTemplateName()
+//                yaml yarnTemplate()
+//            }
+//        }
+//
+//        // 设置整个pipeline 的超时时间为 1个小时
+//
+//        options {
+//            timeout(time:1, unit: 'HOURS')
+//            retry(2)
+//        }
+//
+//        // 添加环境变量
+//        environment {
+//            tags = map.imageTags
+//        }
+//
+//        stages {
+//            stage('Compile') {
+//                steps {
+//                    container('yarn-compile') {
+//                        sh '''
+//                    chmod -R 777 `pwd`
+//                    npm config set registry=http://192.168.3.13:8081/repository/npm/
+//                    npm install
+//                    npm config set registry=http://192.168.3.13:8081/repository/npm/
+//                    npm run build
+//                    '''
+//                    }
+//                }
+//            }
+//
+//            stage('Make image') {
+//                steps {
+//                    container('docker-compose') {
+//                        sh 'docker-compose build'
+//                        sh 'docker-compose push'
+//                    }
+//                }
+//            }
+//
+//            stage('Deploy') {
+//                steps {
+//                    kubernetesDeploy configs: 'Deploy-k8s.yml', kubeConfig: [path: ''], kubeconfigId: 'k8s-deploy-test', secretName: '', ssh: [sshCredentialsId: '*', sshServer: ''], textCredentials: [certificateAuthorityData: '', clientCertificateData: '', clientKeyData: '', serverUrl: 'https://']
+//
+//                }
+//            }
+//
+//        }
+//
+//        post {
+//            always {
+//                echo "over!!"
+//            }
+//
+//            failure {
+//                echo "fail"
+//                script {
+//                    def jobName = env.JOB_NAME.split("/")[0]
+//                    echo jobName
+//                    emailext (
+//                            body: faildBody(jobName),
+//                            subject: 'Jenkins build faild info',
+//                            to: 'zuosheng@dm-ai.cn'
+//                    )
+//                }
+//            }
+//
+//            success {
+//                script {
+//                    def jobName = env.JOB_NAME.split("/")[0]
+//                    echo jobName
+//                    emailext (
+//                            body: showEnv(env, 'success'),
+//                            subject: 'Jenkins build success info',
+//                            to: 'qinyadong@dm-ai.cn'
+//                    )
+//                }
+//            }
+//        }
+//    }
 }
 
 def baseTemplateName() {
