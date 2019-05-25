fc16634e46707801602092e1dbfbb78d43115e8a
左胜 <zuosheng@dm-ai.cn>
2019-05-24 23:56:21 -0700

    test

--- vars/baseFrontend.groovy
+++ vars/baseFrontend.groovy
@@ -34,8 +34,6 @@ def call(Map map, env) {
                         println("【开始进行编译】")
                         sh '''
                             npm config set registry=http://192.168.3.13:8081/repository/npm/
-//                            npm install
-//                            npm run build
                            '''
                     }
                 }
