9a514e2950e7179ab82aa84ad6d787ab6594e54d
左胜 <zuosheng@dm-ai.cn>
2019-05-28 20:04:41 -0700

    test

--- vars/compileNvidiaGpuPrometheusExporter.groovy
+++ vars/compileNvidiaGpuPrometheusExporter.groovy
@@ -4,14 +4,25 @@ def call(Map map, env) {
     pipeline {
         agent none
 
+        options {
+            timeout(time:1, unit: 'HOURS')
+            retry(2)
+        }
+
+        environment {
+            gpuExporterCompilePath = "/root/go/src/github.com/mindprince/nvidia_gpu_prometheus_exporter"
+        }
         stages {
             agent { label 'compile_exporter' }
             stage('Compile') {
                 sh '''
                    yum install -y go
-                   go get github.com/mindprince/nvidia_gpu_prometheus_exporter
-                   cd /root/go/src/github.com/mindprince/nvidia_gpu_prometheus_exporter
-                   go build
+                   echo ${gpuExporterCompilePath}
+                   mkdir -p ${gpuExporterCompilePath}
+                   cp -rp . ${gpuExporterCompilePath}
+                   currentPath=pwd
+                   cd ${gpuExporterCompilePath} && go build
+                   go build -o ${currentPath}/dmai_gpu_exporter
                    '''
             }
         }
