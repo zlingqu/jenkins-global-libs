d707d8ed625f4775687a53f7de00fae5ea53cff1
左胜 <zuosheng@dm-ai.cn>
2019-05-28 20:07:03 -0700

    添加gpu exporter的 编译路径

--- vars/compileNvidiaGpuPrometheusExporter.groovy
+++ vars/compileNvidiaGpuPrometheusExporter.groovy
@@ -13,18 +13,20 @@ def call(Map map, env) {
             gpuExporterCompilePath = "/root/go/src/github.com/mindprince/nvidia_gpu_prometheus_exporter"
         }
         stages {
-            agent { label 'compile_exporter' }
-            stage('Compile') {
-                sh '''
-                   yum install -y go
-                   echo ${gpuExporterCompilePath}
-                   mkdir -p ${gpuExporterCompilePath}
-                   cp -rp . ${gpuExporterCompilePath}
-                   currentPath=pwd
-                   cd ${gpuExporterCompilePath} && go build
-                   go build -o ${currentPath}/dmai_gpu_exporter
-                   '''
-            }
+                stage('Compile') {
+                    agent { label 'compile_exporter' }
+                    steps {
+                        sh '''
+                           yum install -y go
+                           echo ${gpuExporterCompilePath}
+                           mkdir -p ${gpuExporterCompilePath}
+                           cp -rp . ${gpuExporterCompilePath}
+                           currentPath=pwd
+                           cd ${gpuExporterCompilePath} && go build
+                           go build -o ${currentPath}/dmai_gpu_exporter
+                        '''
+                    }
+                }
         }
     }
 }
