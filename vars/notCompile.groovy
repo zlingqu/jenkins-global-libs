93fb116573ba7e581cba6388457d8c5327fb80c8
左胜 <zuosheng@dm-ai.cn>
2019-05-31 03:59:31 -0700

    test

--- vars/notCompile.groovy
+++ vars/notCompile.groovy
@@ -337,6 +337,8 @@ spec:
         args:
         - "--config.file=/workspace/prometheus.yml"
         - "--storage.tsdb.path=/data/tsdb"
+        - "-storage.local.retention"
+        - "168h0m0s"
         resources:
           limits:
             cpu: 3500m
