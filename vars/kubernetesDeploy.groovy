2f8ded0552d6026cbca0c216f00deb2acf0dea23
左胜 <zuosheng@dm-ai.cn>
2019-06-04 00:26:14 -0700

    test

--- vars/kubernetesDeploy.groovy
+++ vars/kubernetesDeploy.groovy
@@ -102,7 +102,7 @@ spec:
         cpu: 2000m
         memory: 4000Mi
   nodeSelector:
-    makeenv: jenkins 
+    makeenv: jenkins
 '''
     def binding = [
             'ansibleImage' :  map.containsKey('ansibleImage') ? map.get('ansibleImage'): 'docker.dm-ai.cn/devops/base-image-exec-ansible:0.02',
