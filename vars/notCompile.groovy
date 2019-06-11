8c90ff3ed717ebdf8c498ae411511ab9930012cf
左胜 <zuosheng@dm-ai.cn>
2019-06-11 02:15:41 -0700

    test

--- vars/notCompile.groovy
+++ vars/notCompile.groovy
@@ -12,37 +12,6 @@ def call(Map map, env) {
     // 注入jenkins的环境变量到全局的Conf
     conf.setJenkinsAttrToConf(env)
 
-
-
-    // 临时的，后面再进行抽取
-    def globalConfig = [
-            'frontend-test' : [
-                    'nodePort': '31377'
-            ],
-            'service-prometheus' : [
-                    'nodePort': '30090',
-                    'namespace': 'devops',
-                    'containerPort': '9090',
-                    'domain': 'http://prometheus.ops.dm-ai.cn',
-                    'kubectlImage': 'devops/base-image-kubectl:0.01'
-            ],
-            'prometheus-alertmanager': [
-                    'namespace': 'devops',
-                    'containerPort': '9093',
-                    'domain': '9093',
-                    'kubectlImage': 'devops/base-image-kubectl:0.01'
-            ],
-            'blackbox-exporter': [
-                    'namespace': 'devops',
-                    'containerPort': '9115',
-                    'domain': '9115',
-                    'nodePort': '30915',
-                    'kubectlImage': 'devops/base-image-kubectl:dev-0.01'
-            ]
-    ]
-
-    map.put('globalConfig', globalConfig)
-
     println('【开始进行构建】')
     pipeline {
         agent {
