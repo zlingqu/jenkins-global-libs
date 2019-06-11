6a591c1b55c9b0b870d943c191ecf5e676068ee0
左胜 <zuosheng@dm-ai.cn>
2019-06-11 00:04:38 -0700

    test

--- vars/notCompile.groovy
+++ vars/notCompile.groovy
@@ -2,7 +2,7 @@ import com.dmai.Conf
 
 def call(Map map, env) {
     // 定义定义的全局的配置项目
-    String appName = map.get('appName')
+    def appName = map.get('appName')
     Conf conf = new Conf(appName, map)
 
     // 临时的，后面再进行抽取
