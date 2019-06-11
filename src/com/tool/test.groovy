289d65cee1335726e256c9af5306129aa4ace000
左胜 <zuosheng@dm-ai.cn>
2019-06-10 23:24:54 -0700

    外部库无法识别java的lib

--- src/com/tool/test.groovy
+++ src/com/tool/test.groovy
@@ -1,13 +1,9 @@
 package com.tool
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 class Test {
 
     Test() {
     }
-
     public static printMap(Map<String, String> printMap) {
         Set<String> key = printMap.keySet()
         for (Iterator<String> it = key.iterator(); it.hasNext();) {
