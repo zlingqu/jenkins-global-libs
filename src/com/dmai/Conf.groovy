05e845e9c6f5b810857330a8c1d5f001e2678265
左胜 <zuosheng@dm-ai.cn>
2019-06-10 23:46:14 -0700

    test

--- src/com/dmai/Conf.groovy
+++ src/com/dmai/Conf.groovy
@@ -1,46 +1,25 @@
 package com.dmai
 
-class GlobalConfig implements Serializable {
-    public Map<String, Map<String, String>> globalConfig = [
-            'frontend-test' : [
-                    'nodePort': '31377'
-            ],
-            'work-attendance-frontend': [
-                    'nodePort': '30800',
-                    'namespace': 'mis'
-            ],
-            'mis-admin-frontend': [
-                    'nodePort': '30090',
-                    'namespace': 'mis'
-            ],
-            'org-frontend': [
-                    'nodePort': '30091',
-                    'namespace': 'mis'
-            ],
-            'mis-admin-backend': [
-                    'nodePort': '31500',
-                    'namespace': 'mis',
-                    'appPort': '5000'
-            ],
-            'service-prometheus' : [
-                    'nodePort': '30090',
-                    'namespace': 'devops',
-                    'containerPort': '9090',
-                    'domain': 'http://prometheus.ops.dm-ai.cn',
-                    'kubectlImage': 'devops/base-image-kubectl:0.01'
-            ],
-            'prometheus-alertmanager': [
-                    'namespace': 'devops',
-                    'containerPort': '9093',
-                    'domain': '9093',
-                    'kubectlImage': 'devops/base-image-kubectl:0.01'
-            ],
-            'blackbox-exporter': [
-                    'namespace': 'devops',
-                    'containerPort': '9115',
-                    'domain': '9115',
-                    'nodePort': '30915',
-                    'kubectlImage': 'devops/base-image-kubectl:dev-0.01'
-            ]
-    ]
+class Conf implements Serializable{
+
+    private Map<String, Map<String, String>> conf
+    private Map<String, String> userSetMap
+
+    // init
+    Conf(String appName, Map<String, String> userSetMap, Map<String, String> jenkinsEnv) {
+        this.conf = new GlobalConfig().globalConfig
+        this.userSetMap = userSetMap
+        this.setUserAttr(appName, userSetMap, jenkinsEnv)
+    }
+
+    // get attr
+    public GetAttr(String appName, String attrName) {
+        return this.conf.get(appName).get(attrName)
+    }
+
+    // set user attr
+    private setUserAttr(String appName, Map<String, String> userMap, Map<String, String> jenkinsEnv) {
+        this.conf.get(appName).putAll(userMap)
+        this.conf.get(appName).putAll(jenkinsEnv)
+    }
 }
---
+++ src/com/dmai/GlobalConfig.groovy
@@ -0,0 +1,46 @@
+package com.dmai
+
+class GlobalConfig implements Serializable {
+    public Map<String, Map<String, String>> globalConfig = [
+            'frontend-test' : [
+                    'nodePort': '31377'
+            ],
+            'work-attendance-frontend': [
+                    'nodePort': '30800',
+                    'namespace': 'mis'
+            ],
+            'mis-admin-frontend': [
+                    'nodePort': '30090',
+                    'namespace': 'mis'
+            ],
+            'org-frontend': [
+                    'nodePort': '30091',
+                    'namespace': 'mis'
+            ],
+            'mis-admin-backend': [
+                    'nodePort': '31500',
+                    'namespace': 'mis',
+                    'appPort': '5000'
+            ],
+            'service-prometheus' : [
+                    'nodePort': '30090',
+                    'namespace': 'devops',
+                    'containerPort': '9090',
+                    'domain': 'http://prometheus.ops.dm-ai.cn',
+                    'kubectlImage': 'devops/base-image-kubectl:0.01'
+            ],
+            'prometheus-alertmanager': [
+                    'namespace': 'devops',
+                    'containerPort': '9093',
+                    'domain': '9093',
+                    'kubectlImage': 'devops/base-image-kubectl:0.01'
+            ],
+            'blackbox-exporter': [
+                    'namespace': 'devops',
+                    'containerPort': '9115',
+                    'domain': '9115',
+                    'nodePort': '30915',
+                    'kubectlImage': 'devops/base-image-kubectl:dev-0.01'
+            ]
+    ]
+}
