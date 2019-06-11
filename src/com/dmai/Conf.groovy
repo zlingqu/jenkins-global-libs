6985a895e3f325a7a3debc00e03222ef23e6c245
左胜 <zuosheng@dm-ai.cn>
2019-06-11 00:07:45 -0700

    test

--- src/com/dmai/Conf.groovy
+++ src/com/dmai/Conf.groovy
@@ -2,15 +2,15 @@ package com.dmai
 
 class Conf implements Serializable{
 
-    private Map<String, Map<String, String>> conf
-    private Map<String, String> userSetMap
     private String appName
+    private Map<String, String> userSetMap
+    private Map<String, Map<String, String>> conf
 
     Conf(String appName, Map<String, String> userSetMap) {
         this.appName = appName
         this.userSetMap = userSetMap
         this.conf = new GlobalConfig().globalConfig
-        this.setUserAttr(userSetMap)
+//        this.setUserAttr(userSetMap)
     }
 
     // get attr
