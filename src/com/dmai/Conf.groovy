dab0775658a48fe4821b378717ef97a9f708b88b
左胜 <zuosheng@dm-ai.cn>
2019-06-11 00:19:42 -0700

    test

--- src/com/dmai/Conf.groovy
+++ src/com/dmai/Conf.groovy
@@ -4,12 +4,12 @@ class Conf implements Serializable{
 
     private String appName
     private Map<String, String> userSetMap
-    private Map<String, Map<String, String>> conf
+    private Map<String, String> appConf
 
     Conf(String appName, Map<String, String> userSetMap) {
         this.appName = appName
         this.userSetMap = userSetMap
-        this.conf = new GlobalConfig().globalConfig
+        this.appConf = new GlobalConfig().globalConfig.get(appName)
         this.setUserAttr(userSetMap)
     }
 
@@ -20,6 +20,7 @@ class Conf implements Serializable{
 
     // set user attr
     private setUserAttr(Map<String, String> userMap) {
-        this.conf.get(this.appName).putAll(userMap)
+
+        this.appConf.putAll(userMap)
     }
 }
