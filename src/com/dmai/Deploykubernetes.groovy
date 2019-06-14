e6266ca5039ef56c34afd098ad5baf17d5da56b8
左胜 <zuosheng@dm-ai.cn>
2019-06-13 20:37:31 -0700

    updateconfigmap

--- src/com/dmai/Deploykubernetes.groovy
+++ src/com/dmai/Deploykubernetes.groovy
@@ -42,7 +42,7 @@ class Deploykubernetes {
 
             default:
                 this.script.sh String.format("kubectl delete configmap %s -n %s || echo 0", this.conf.appName, this.conf.getAttr('namespace'))
-                this.script.sh String.format("kubectl create configmap %s --from-file=%s=env/%s.%s -n %s",
+                this.script.sh String.format("kubectl create configmap %s --from-file=%s=%s -n %s",
                 this.conf.appName, this.conf.getAttr('configMapName'),this.conf.getAttr('branchName'), this.conf.getAttr('configMapName'),this.conf.getAttr('namespace'))
         }
     }
