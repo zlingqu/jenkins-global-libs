4d7f2b32a3d951c416ff6b615b30f7139488f2f8
左胜 <zuosheng@dm-ai.cn>
2019-06-24 22:32:21 -0700

    test

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -67,6 +67,14 @@ def call(Map map, env) {
             }
 
             stage('Make Image') {
+                input {
+                    message "Should we continue?"
+                    ok "Yes, we should."
+                    submitter "alice,bob"
+                    parameters {
+                        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                    }
+                }
                 steps {
                     container('docker-compose') {
                         script {
