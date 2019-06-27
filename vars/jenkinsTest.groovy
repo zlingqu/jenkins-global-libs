926044db284f7f35e3f2debfd07e35d4ae00ee99
左胜 <zuosheng@dm-ai.cn>
2019-06-27 02:11:33 -0700

    test

--- vars/jenkinsTest.groovy
+++ vars/jenkinsTest.groovy
@@ -23,6 +23,7 @@ def call(Map map, env) {
             string(name: 'BROWSER_TYPE', defaultValue: 'chrome', description: 'Type a browser type, should be chrome/firefox')
             string(name: 'TEST_SERVER_URL', defaultValue: '', description: 'Type the test server url')
             string(name: 'NODE', defaultValue: 'win-anthony-demo', description: 'Please choose a windows node to execute this job.')
+            string(name: 'devDeployEnv', defaultValue: 'dev', description: 'Please choose a windows node to execute this job.')
         }
 
         environment {
