e159d98428ccc4bfef74517572ca5c3f4b39f0e1
左胜 <zuosheng@dm-ai.cn>
2019-06-30 22:32:38 -0700

    修改编译的时候的node_modules目录的链接

--- src/com/dmai/Compile.groovy
+++ src/com/dmai/Compile.groovy
@@ -15,9 +15,8 @@ class Compile {
             switch (this.conf.getAttr('codeLanguage')) {
                 case 'js':
                     this.script.sh "test -d node_modules && rm -fr node_modules;" +
-                            " cp -rp /data/cache/node_modules ./;" +
-                            "npm config set registry=http://192.168.3.13:8081/repository/npm/ && npm install && npm run build" +
-                            "; cp -rp node_modules /data/cache/node_modules"
+                            " ln -s /data/cache/node_modules node_modules;" +
+                            "npm config set registry=http://192.168.3.13:8081/repository/npm/ && npm install && npm run build"
                     return
                 case 'c++':
                     this.script.sh "make"
