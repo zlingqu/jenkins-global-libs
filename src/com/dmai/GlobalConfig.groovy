1346f1ef6e7f1590e870f20d37847875015eeac4
左胜 <zuosheng@dm-ai.cn>
2019-07-09 23:42:00 -0700

    修改mis-admin的参数

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -517,13 +517,13 @@ class GlobalConfig implements Serializable {
                     'namespace': 'mis',
                     'nodePort': '30092',
                     'containerPort': '80',
-                    'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+                    'domain': 'hr.mis.dm-ai.cn', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
 //                    'cpuRequests' : '200m',
 //                    'memoryRequests' : '400Mi',
 //                    'cpuLimits' : '400m',
 //                    'memoryLimits' : '1000Mi',
-                    'dev': 'test', // dev分支部署到开发环境
-                    'test': false, // 是否从dev分支部署到测试环境
+                    'dev': 'dev', // dev分支部署到开发环境
+                    'test': true, // 是否从dev分支部署到测试环境
                     'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
                     'gitAddress': 'https://gitlab.dm-ai.cn/mis/admin/mis-admin-frontend.git',
                     'compile': true, // 是否编译
@@ -766,8 +766,8 @@ class GlobalConfig implements Serializable {
                     'memoryRequests' : '400Mi',
                     'cpuLimits' : '400m',
                     'memoryLimits' : '1000Mi',
-                    'dev': 'test', // dev分支部署到开发环境
-                    'test': false, // 是否从dev分支部署到测试环境
+                    'dev': 'dev', // dev分支部署到开发环境
+                    'test': true, // 是否从dev分支部署到测试环境
                     'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
                     'gitAddress': 'https://gitlab.dm-ai.cn/mis/admin/mis-admin-backend.git',
                     'compile': false, // 是否编译
