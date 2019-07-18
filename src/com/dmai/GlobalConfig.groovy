c2d7e149b6b9439b397b8d2fe73e0c927e54b10c
左胜 <zuosheng@dm-ai.cn>
2019-07-18 02:21:54 -0700

    test

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1028,8 +1028,8 @@ class GlobalConfig implements Serializable {
                     'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
 //                    'cpuRequests' : '1000m',
 //                    'memoryRequests' : '2000Mi',
-//                    'cpuLimits' : '1000m',
-//                    'memoryLimits' : '2000Mi',
+                    'cpuLimits' : '1000m',
+                    'memoryLimits' : '2000Mi',
                     'replicas' : 1,
                     'dev': 'dev', // dev分支部署到测试环境
                     'test': true, // 是否从dev分支部署到测试环境
