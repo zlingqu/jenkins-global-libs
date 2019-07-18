776c02b99f5457b4de1abff016d1ff9f4d8a6684
左胜 <zuosheng@dm-ai.cn>
2019-07-17 19:27:24 -0700

    修改项目mis-admin-frontend 使用自定义的dockerfile

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -579,7 +579,7 @@ class GlobalConfig implements Serializable {
                     'gitAddress': 'https://gitlab.dm-ai.cn/mis/admin/mis-admin-frontend.git',
                     'compile': true, // 是否编译
                     'deploy': true, // 是否自动化部署
-                    'customDockerfile': false, // 是否使用自定义 dockerfile
+                    'customDockerfile': true, // 是否使用自定义 dockerfile
                     'customKubernetesDeployTemplate' : false, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
                     'useConfigMap': false, //是否使用configmap
                     'svcType' : 'NodePort', // ['ClusterIP', 'NodePort', 'None']
