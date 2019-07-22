e25438d88e8c24d483615767a8ce0e4c731a86bf
左胜 <zuosheng@dm-ai.cn>
2019-07-22 04:25:26 -0700

    添加sonar-check的步骤

---
+++ src/com/dmai/CodeCheck.groovy
@@ -0,0 +1,30 @@
+package com.dmai
+
+class CodeCheck {
+    protected final def script
+    private final Conf conf
+
+    CodeCheck( script, Conf conf ){
+        this.script = script
+        this.conf = conf
+    }
+
+    // code check for sonar
+    public void sonarCheck(){
+        try {
+            this.script.sh String.format("/sonar-scanner/bin/sonar-scanner -Dsonar.host.url=http://sonar.ops.dm-ai.cn -Dsonar.login=admin -Dsonar.password=33f5b945a8b5908793b6e -Dsonar.language=%s -Dsonar.projectName=%s -Dsonar.projectVersion=1.0 -Dsonar.sourceEncoding=UTF-8 -Dsonar.projectKey=%s -Dsonar.exclusions=**/node_modules/** -Dsonar.sources=./ -Dsonar.projectBaseDir=%s",
+            this.getCodeLanguage(),
+            this.conf.appName,
+            this.conf.appName,
+            '/home/jenkins/workspace/' + this.conf.appName + '_' + this.conf.getAttr('branchName'))
+        }
+        catch (e) {
+            this.script.sh "${e}"
+        }
+    }
+
+    private String getCodeLanguage() {
+        if (this.conf.getAttr('codeLanguage') == 'node') return 'js'
+        return this.conf.getAttr('codeLanguage')
+    }
+}
