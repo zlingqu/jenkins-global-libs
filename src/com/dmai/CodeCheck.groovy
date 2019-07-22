de773724627b4a7320543ac47874179c5820d97b
左胜 <zuosheng@dm-ai.cn>
2019-07-22 05:03:01 -0700

    test

--- src/com/dmai/CodeCheck.groovy
+++ src/com/dmai/CodeCheck.groovy
@@ -12,7 +12,7 @@ class CodeCheck {
     // code check for sonar
     public void sonarCheck(){
         try {
-            this.script.sh  'sleep 1000'
+//            this.script.sh  'sleep 1000'
             this.script.sh String.format("/sonar-scanner/bin/sonar-scanner -Dsonar.host.url=http://sonar.ops.dm-ai.cn -Dsonar.login=admin -Dsonar.password=33f5b945a8b5908793b6e -Dsonar.language=%s -Dsonar.projectName=%s -Dsonar.projectVersion=1.0 -Dsonar.sourceEncoding=UTF-8 -Dsonar.projectKey=%s -Dsonar.exclusions=**/node_modules/** -Dsonar.sources=./ -Dsonar.projectBaseDir=%s",
             this.getCodeLanguage(),
             this.conf.appName,
