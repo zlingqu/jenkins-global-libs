e23cd2f81adb99f765cecfeb764618e350ec107d
左胜 <zuosheng@dm-ai.cn>
2019-07-25 05:31:50 -0700

    test

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -56,7 +56,7 @@ def call(Map map, env) {
                 defaultContainer 'jnlp'
                 namespace 'devops'
                 inheritFrom 'base-template'
-                yaml new JenkinsRunTemplate(conf).getJenkinsRunTemplate(vueAppScene)
+                yaml new JenkinsRunTemplate(conf).getJenkinsRunTemplate(params.VUE_APP_SCENE)
             }
         }
 
