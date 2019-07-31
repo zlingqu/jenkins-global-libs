b3192db29bbdfdb28ae87c59f45259ce48b76c7b
左胜 <zuosheng@dm-ai.cn>
2019-07-31 02:21:19 -0700

    修改域名

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -10,7 +10,7 @@ class GlobalConfig implements Serializable {
                     'namespace': 'mis',
                     'nodePort': '30800',
                     'containerPort': '80',
-                    'domain': 'wattdev.mis.dm-ai.cn', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+                    'domain': 'watt.mis.dm-ai.cn', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
 //                    'cpuRequests' : '200m',
 //                    'memoryRequests' : '400Mi',
                     'cpuLimits' : '500m',
