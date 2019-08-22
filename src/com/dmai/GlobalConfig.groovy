89b8927faf57f650ec4803c1252d64eb345d756a
左胜 <zuosheng@dm-ai.cn>
2019-08-22 12:44:19 +0800

    添加项目cp2-aog

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1970,6 +1970,34 @@ class GlobalConfig implements Serializable {
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
                     'sonarCheck'  : true
             ],
+            'cp2-aog': [
+                    'servicePort' : '80',
+                    'namespace': 'devops',
+//                    'nodePort': '30092',
+                    'containerPort': '80',
+                    'domain': 'cp2.dm-ai.cn', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+//                    'cpuRequests' : '200m',
+//                    'memoryRequests' : '400Mi',
+                    'cpuLimits' : '200m',
+                    'memoryLimits' : '400Mi',
+                    'dev': 'dev', // dev分支部署到开发环境
+                    'test': true, // 是否从dev分支部署到测试环境
+                    'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
+                    'gitAddress': 'https://gitlab.dm-ai.cn/wangqing/aog-gui.git',
+                    'compile': true, // 是否编译
+                    'deploy': true, // 是否自动化部署
+                    'customDockerfile': false, // 是否使用自定义 dockerfile
+                    'customKubernetesDeployTemplate' : false, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
+                    'useConfigMap': false, //是否使用configmap
+                    'svcType' : 'ClusterIP', // ['ClusterIP', 'NodePort', 'None']
+                    'codeLanguage' : 'js', // 临时的，默认是【js,node,golang,java,php,python】
+                    'k8sKind': 'deployment', // 部署的服务的类型
+                    'configMapName': 'config.env', //是否使用configmap
+                    'useStore': false, // 是否使用存储资源。
+                    'useService': true, // 是否使用service
+                    'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
+                    'sonarCheck'  : true
+            ],
             'app-deploy-platform-env-manger': [
                     'servicePort' : '80',
                     'namespace': 'devops',
