09f915b695caf6751a4174b79b43c4b569c59fc1
左胜 <zuosheng@dm-ai.cn>
2019-08-26 18:33:04 +0800

    test

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1671,7 +1671,7 @@ class GlobalConfig implements Serializable {
                     'namespace': 'x2-ta',
                     'nodePort' : '31222',
                     'containerPort': '8888',
-                    'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+                    'domain': 'admin-server-x2.dm-ai.cn', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
 //                    'cpuRequests' : '1000m',
 //                    'memoryRequests' : '2000Mi',
                     'cpuLimits' : '500m',
@@ -1702,7 +1702,7 @@ class GlobalConfig implements Serializable {
                     'namespace': 'x2-ta',
                     'nodePort' : '31227',
                     'containerPort': '3000',
-                    'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+                    'domain': 'stats-server-x2.dm-ai.cn', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
 //                    'cpuRequests' : '1000m',
 //                    'memoryRequests' : '2000Mi',
                     'cpuLimits' : '500m',
