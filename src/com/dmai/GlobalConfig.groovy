842464da3824ef5d03f47f36002d84db68f264f2
左胜 <zuosheng@dm-ai.cn>
2019-08-27 09:59:59 +0800

    添加项目prometheus-pushgateway

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1442,6 +1442,28 @@ class GlobalConfig implements Serializable {
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
                     'sonarCheck'  : true
             ],
+            'prometheus-pushgateway': [
+                    'servicePort' : '9091',
+                    'namespace': 'devops',
+                    'nodePort' : '30991',
+                    'containerPort': '9091',
+                    'dev': 'dev', // dev分支部署到开发环境
+                    'test': true, // 是否从dev分支部署到测试环境
+                    'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
+                    'gitAddress': 'https://gitlab.dm-ai.cn/application-engineering/devops/pushgateway.git',
+                    'kubectlImage': 'devops/base-image-kubectl:0.01',
+                    'compile': false, // 是否编译
+                    'deploy': true, // 是否自动化部署
+                    'customDockerfile': true, // 是否使用自定义 dockerfile
+                    'customKubernetesDeployTemplate' : false, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
+                    'useConfigMap': false, //是否使用configmap
+                    'svcType' : 'ClusterIP', // ['ClusterIP', 'NodePort', 'None']
+                    'codeLanguage' : 'golang', // 临时的，默认是【js,node,golang,java,php,python】
+                    'k8sKind': 'deployment', // 部署的服务的类型
+                    'useStore': false, // 是否使用存储资源。
+                    'useService': true, // 是否使用service
+                    'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
+            ],
             'xmc2-test-deploy': [
                     'servicePort' : '80',
                     'nodePort': '31500',
