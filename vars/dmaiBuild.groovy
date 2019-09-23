0ac233ae6b9d424e354154a3a44c7082b901388f
左胜 <zuosheng@dm-ai.cn>
2019-09-23 15:58:23 +0800

    test

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -89,6 +89,7 @@ def call(Map map, env) {
             choice(name: 'NODE_ENV', choices: ['dev', 'prod', 'test', 'stage'], description: '前端专用，其他不关注')
 
             string(name: 'DEPLOY_MASTER_PASSWORD', defaultValue: '部署master分支到线上环境，请找运维人员输入密码自动部署', description: '部署master分支请找运维人员输入密码自动部署')
+            string(name: 'COMPILE_PARAM', defaultValue: '', description: 'android项目自定义的编译参数')
 
             string(name: 'REPLICAS', defaultValue: replicas, description: '部署在k8s集群中需要的副本数')
 
@@ -200,6 +201,9 @@ def call(Map map, env) {
 
                         // 前端专用
                         conf.setAttr('nodeEnv', params.NODE_ENV)
+
+                        // set android param
+                        conf.setAttr('compileParam', params.COMPILE_PARAM)
 
                     }
                 }
