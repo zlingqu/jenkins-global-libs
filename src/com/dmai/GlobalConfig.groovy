1aabb1426beeb4f3ab133afea1d1bede2ad30d61
dev-admin <dev-admin@dm-ai.cn>
2019-09-29 12:16:13 +0800

    修改pipeline

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -833,13 +833,14 @@ class GlobalConfig implements Serializable {
                     'customDockerfile': true, // 是否使用自定义 dockerfile
                     'customKubernetesDeployTemplate' : false, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
                     'useConfigMap': true, //是否使用configmap
-                    'configMapName': 'config.yaml', //是否使用configmap
+                    'configMapName': '', //是否使用configmap
                     'svcType' : 'NodePort', // ['ClusterIP', 'NodePort', 'None']
                     'codeLanguage' : 'python', // 临时的，默认是【js,node,golang,java,php,python】
                     'k8sKind': 'deployment', // 部署的服务的类型
                     'useStore': false, // 是否使用存储资源。
                     'useService': true, // 是否使用service
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
+                    'useEnvFile'  : true, // 是否使用git仓库deployment下的configmap.yaml的内容来给容器注入环境变量。
                     'envType'     : 'gpu', //分为gpu 和非gpu的环境
                     'sonarCheck'  : true
             ],
