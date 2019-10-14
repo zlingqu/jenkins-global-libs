5d63b6f494ebcb92510f24f110be8698cae92e68
dev-admin <dev-admin@dm-ai.cn>
2019-10-14 10:32:34 +0800

    添加xmc2-proxy-service项目

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -118,6 +118,35 @@ class GlobalConfig implements Serializable {
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
                     'sonarCheck'  : true
             ],
+             'xmc2-proxy-service': [
+                    'servicePort' : '80',
+                    'namespace': 'xmc2-lexue',
+                    'nodePort' : '30227',
+                    'containerPort': '3000',
+                    'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+//                    'cpuRequests' : '300m',
+//                    'memoryRequests' : '500Mi',
+//                    'cpuLimits' : '800m',
+//                    'memoryLimits' : '1000Mi',
+                    'replicas' : 2,
+                    'dev': 'dev', // dev分支部署到测试环境
+                    'test': true, // 是否从dev分支部署到测试环境
+                    'master': 'prddd', // 如果参数master 不等于prd，整个构建就失败，---
+                    'gitAddress': 'https://gitlab.dm-ai.cn/XMC/xmc2/ui-backend-service.git',
+                    'compile': false, // 是否编译
+                    'deploy': true, // 是否自动化部署
+                    'customDockerfile': true, // 是否使用自定义 dockerfile
+                    'customKubernetesDeployTemplate' : true, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
+                    'useConfigMap': true, //是否使用configmap
+                    'configMapName': 'config.js', //是否使用configmap
+                    'svcType' : 'NodePort', // ['ClusterIP', 'NodePort', 'None']
+                    'codeLanguage' : 'node', // 临时的，默认是【js,node,golang,java,php,python】
+                    'k8sKind': 'deployment', // 部署的服务的类型
+                    'useStore': false, // 是否使用存储资源。
+                    'useService': true, // 是否使用service
+                    'makeImage'   : false, // 是否进行镜像的构造，打镜像，push镜像
+                    'sonarCheck'  : false
+            ],
             'ui-backend-service': [
                     'servicePort' : '80',
                     'namespace': 'xmc2-lexue',
