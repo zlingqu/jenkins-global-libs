36ed1ceea762e10b57ae842cfee463da56196787
dev-admin <dev-admin@dm-ai.cn>
2019-10-17 10:36:50 +0800

    添加tx2-stat-service

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1278,6 +1278,36 @@ class GlobalConfig implements Serializable {
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
                     'sonarCheck'  : true
             ],
+			'tx2-stat-service': [
+                    'servicePort' : '80',
+                    'namespace': 'tx2',
+                    'nodePort' : '31165',
+                    'containerPort': '3000',
+                    'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+                    'cpuRequests' : '',
+                    'memoryRequests' : '',
+                    'cpuLimits' : '',
+                    'memoryLimits' : '',
+                    'replicas' : 3,
+                    'dev': 'dev', // dev分支部署到开发环境
+                    'test': true, // 是否从dev分支部署到测试环境
+                    'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
+                    'gitAddress': 'https://gitlab.dm-ai.cn/XMC/xmc-tx2/stat-service.git',
+                    'compile': false, // 是否编译
+                    'deploy': false, // 是否自动化部署
+                    'customDockerfile': true, // 是否使用自定义 dockerfile
+                    'customKubernetesDeployTemplate' : false, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
+                    'useConfigMap': true, //是否使用configmap
+                    'svcType' : 'NodePort', // ['ClusterIP', 'NodePort', 'None']
+                    'codeLanguage' : 'node', // 临时的，默认是【js,node,golang,java,php,python】
+                    'k8sKind': 'deployment', // 部署的服务的类型
+                    'configMapName': 'config.js', //是否使用configmap
+                    'useStore': true, // 是否使用存储资源。
+                    'storePath' : '/app/data',
+                    'useService': false, // 是否使用service
+                    'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
+                    'sonarCheck'  : true
+            ],
             'xmc-offline-task': [
                     'servicePort' : '80',
                     'namespace': 'xmc',
