2c025d6c12321df1f54c0343103b12c0dec5a646
dev-admin <dev-admin@dm-ai.cn>
2019-10-25 14:00:48 +0800

    添加项目

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1539,7 +1539,7 @@ class GlobalConfig implements Serializable {
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
                     'sonarCheck'  : true
             ],
-             'tk-engine-image-process': [
+             'tk-engine-image-process-student': [
                     'servicePort' : '80',
                     'namespace': 'xmc2-tk',
 //                    'nodePort' : '31165',
@@ -1566,6 +1566,40 @@ class GlobalConfig implements Serializable {
                     'codeLanguage' : 'python', // 临时的，默认是【js,node,golang,java,php,python】
                     'k8sKind': 'deployment', // 部署的服务的类型
                     'command': 'command: ["python", "main.py", "--config", "config-student.yaml"]', //自定义pod启动的命令行
+//                    'configMapName': 'config.js', //是否使用configmap
+                    'useStore': false, // 是否使用存储资源。
+                    'useService': false, // 是否使用service
+                    'useEnvFile'  : true, // 是否使用git仓库deployment下的.env的内容来给容器注入环境变量。
+                    'envType'     : 'gpu', //分为gpu 和非gpu的环境
+                    'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
+            ],
+            'tk-engine-image-process-teacher': [
+                    'servicePort' : '80',
+                    'namespace': 'xmc2-tk',
+//                    'nodePort' : '31165',
+                    'containerPort': '3000',
+                    'domain': '', // domain为空，或者没有这条属性，则邮件不发送域名，否则给用户发送域名地址。
+                    'cpuRequests' : '',
+                    'memoryRequests' : '',
+                    'cpuLimits' : '',
+                    'memoryLimits' : '',
+                    'gpuLimits' : 1, //一个副本使用几张gpu的卡
+                    'replicas' : 1,
+                    'dev': 'dev', // dev分支部署到开发环境
+                    'test': true, // 是否从dev分支部署到测试环境
+                    'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
+                    'gitAddress': 'https://gitlab.dm-ai.cn/XMC/xmc-tk/xmc-engine-image-process.git',
+                    'useModel': true,
+                    'modelGitAddress': 'https://gitlab.dm-ai.cn/XMC/xmc-tk/xmc-models.git', //请按此格式书写，http，不要git@
+                    'compile': false, // 是否编译
+                    'deploy': true, // 是否自动化部署
+                    'customDockerfile': true, // 是否使用自定义 dockerfile
+                    'customKubernetesDeployTemplate' : false, // 是否使用用户自定义的k8s部署文件，默认文件名为：Deploy-k8s.yml
+                    'useConfigMap': true, //是否使用configmap
+                    'svcType' : 'ClusterIP', // ['ClusterIP', 'NodePort', 'None']
+                    'codeLanguage' : 'python', // 临时的，默认是【js,node,golang,java,php,python】
+                    'k8sKind': 'deployment', // 部署的服务的类型
+                    'command': 'command: ["python", "main.py", "--config", "config-teacher.yaml"]', //自定义pod启动的命令行
 //                    'configMapName': 'config.js', //是否使用configmap
                     'useStore': false, // 是否使用存储资源。
                     'useService': false, // 是否使用service
