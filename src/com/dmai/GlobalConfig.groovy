3feaffdd6d4d666de299d8d6117786dd914a37af
dev-admin <dev-admin@dm-ai.cn>
2019-10-30 09:59:50 +0800

    修改项目

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1742,7 +1742,7 @@ class GlobalConfig implements Serializable {
                     'dev': 'dev', // dev分支部署到测试环境
                     'test': true, // 是否从dev分支部署到测试环境
                     'master': 'prd', // 如果参数master 不等于prd，整个构建就失败，---
-                    'gitAddress': 'https://gitlab.dm-ai.cn/XMC/xmc-tk/xmc-user-validation.git',
+                    'gitAddress': 'https://gitlab.dm-ai.cn/XMC/xmc-tk/user-validation.git',
                     'stage': false, //是否部署到stage环境
                     'compile': false, // 是否编译
                     'deploy': true, // 是否自动化部署
