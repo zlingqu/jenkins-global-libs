6ef35a7a4fefaf2efbe0b2e79fc7b6a8b68f96fc
quzhongling <quzhongling@dm-ai.cn>
2019-11-07 21:51:53 +0800

    Update GlobalConfig.groovy

--- src/com/dmai/GlobalConfig.groovy
+++ src/com/dmai/GlobalConfig.groovy
@@ -1974,8 +1974,7 @@ class GlobalConfig implements Serializable {
                     'svcType' : 'ClusterIP', // ['ClusterIP', 'NodePort', 'None']
                     'codeLanguage' : 'node', // 临时的，默认是【js,node,golang,java,php,python】
                     'k8sKind': 'deployment', // 部署的服务的类型
-                    'storage': false,  //是否需要挂载存储
-                    'useStore': false, // 是否使用存储资源
+                    'useStore': true, // 是否使用存储资源
                     'storePath' : '/data',
                     'useService': true, // 是否使用service
                     'makeImage'   : true, // 是否进行镜像的构造，打镜像，push镜像
