6f12cf645e40bfcd668151318c746e7903154ebb
左胜 <zuosheng@dm-ai.cn>
2019-11-27 17:22:41 +0800

    test

--- src/com/dmai/Compile.groovy
+++ src/com/dmai/Compile.groovy
@@ -60,7 +60,7 @@ class Compile {
                 case 'android':
                     this.script.sh "test -e /cache/.gradle && rm -fr /cache/.gradle; test -e /root/.gradle && rm -fr /root/.gradle; " +
                             "test -e /android_cache/.gradle-cache.tar && cp -rp /android_cache/.gradle-cache.tar /cache && tar xf /cache/.gradle-cache.tar -C /cache;" +
-                            "test -e /android_cache/.gradle-root.tar && cp -rp /android_cache/.gradle-root.tar /cache && tar xf /cache/.gradle-root.tar -C /root;" +
+                            "test -e /android_cache/.gradle-root.tar && cp -rp /android_cache/.gradle-root.tar /root && tar xf /root/.gradle-root.tar -C /root;" +
                             "bash -x compile.sh  " + "${this.conf.getAttr('compileParam')} && " +
                             "cd /cache && tar cf .gradle-cache.tar .gradle && mv .gradle-cache.tar /android_cache && " +
                             "cd /root && tar cf .gradle-root.tar .gradle && mv .gradle-root.tar /android_cache"
