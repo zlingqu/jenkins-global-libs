27390a8a129d871285445e782968a621119f8cbf
左胜 <zuosheng@dm-ai.cn>
2019-12-03 17:45:05 +0800

    支持tsc

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -523,7 +523,7 @@ spec:
             case 'node':
                 return String.format('''
   - name: compile
-    image: docker.dm-ai.cn/devops/node:0.03
+    image: docker.dm-ai.cn/devops/node:0.0.4
     imagePullPolicy: IfNotPresent
     env: #指定容器中的环境变量
     - name: DMAI_PRIVATE_DOCKER_REGISTRY
