4f8f9fb96c76172476da71864246d144a9ca6b6e
左胜 <zuosheng@dm-ai.cn>
2019-12-30 14:31:00 +0800

    test

--- src/com/dmai/DockerFileTemplate.groovy
+++ src/com/dmai/DockerFileTemplate.groovy
@@ -83,7 +83,7 @@ ADD nginx.conf /etc/nginx/conf.d/default.conf
 RUN ln -sf /dev/stdout /var/log/nginx/access.log
 RUN ln -sf /dev/stderr /var/log/nginx/error.log
 EXPOSE 80
-ENTRYPOINT nginx -g "daemon off;"
+CMD nginx -g "daemon off;"
 '''
     }
 
