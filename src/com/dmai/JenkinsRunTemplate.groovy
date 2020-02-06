a55ec0520760bdc1f8756c42771916db287c400b
左胜 <zuosheng@dm-ai.cn>
2020-02-06 20:38:21 +0800

    去掉不需要的参数-VUE_APP_SCHOOL

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -20,21 +20,13 @@ class JenkinsRunTemplate {
             }
             this.conf.setUserAttr(new GlobalConfig().globalConfig.get(this.conf.appName))
         } else if (this.conf.appName == 'xmc2-frontend') {
-            this.conf.setAppName(this.conf.appName + (params.VUE_APP_SCENE == 'school' ? '' : '-' + params.VUE_APP_SCENE) + (params.VUE_APP_SCHOOL == 'S00001' ? '' : '-' + params.VUE_APP_SCHOOL)  )
+            this.conf.setAppName(this.conf.appName + (params.VUE_APP_SCENE == 'school' ? '' : '-' + params.VUE_APP_SCENE))
             this.conf.setVueAppScene(params.VUE_APP_SCENE)
-            this.conf.setVueAppSchool(params.VUE_APP_SCHOOL)
         } else {
             if (params.APP_NAME != 'xmc-xc-model-serving' && params.APP_NAME != 'xmc-model-serving-student') {
                 this.conf.setAppName(params.APP_NAME)
             }
         }
-
-//        if (this.conf.appName == 'xmc2-frontend') {
-//            this.conf.setAppName(this.conf.appName + (params.VUE_APP_SCENE == 'school' ? '' : '-' + params.VUE_APP_SCENE) + (params.VUE_APP_SCHOOL == 'S00001' ? '' : '-' + params.VUE_APP_SCHOOL)  )
-//            this.conf.setVueAppScene(params.VUE_APP_SCENE)
-//            this.conf.setVueAppSchool(params.VUE_APP_SCHOOL)
-//
-//        }
 
         this.conf.setAttr('replicas', params.REPLICAS)
 
