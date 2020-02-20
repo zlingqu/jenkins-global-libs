7f286fb3c83ea4ad1a670731b6e3beebfa88c6f6
左胜 <zuosheng@dm-ai.cn>
2020-02-20 17:46:19 +0800

    修改分支名的特殊符号

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -3,7 +3,7 @@ import com.tool.Tools
 
 def call(Map map, env) {
     // 默认master 和 dev分支才进行构建
-    if (!(env.BRANCH_NAME in ['master', 'dev', 'stage','release'])) return
+//    if (!(env.BRANCH_NAME in ['master', 'dev', 'stage','release'])) return
 
     // 定义定义的全局的配置项目
     String appName = map.get('appName')
