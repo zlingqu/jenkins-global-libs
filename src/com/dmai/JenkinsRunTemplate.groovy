d25a90e2923927557026d26a3623779f5811ff5a
左胜 <zuosheng@dm-ai.cn>
2020-02-27 22:33:27 +0800

    test

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -41,9 +41,9 @@ class JenkinsRunTemplate {
         // set GPU_CARD_COUNT
         this.conf.setAttr('gpuLimits', params.GPU_CARD_COUNT)
 
-        if (this.conf.getAttr('deployEnv') == 'lexue') {
-            this.conf.setockerRegistryHost('rdac-docker.dm-ai.cn')
-        }
+//        if (this.conf.getAttr('deployEnv') == 'lexue') {
+//            this.conf.setockerRegistryHost('rdac-docker.dm-ai.cn')
+//        }
 
         this.conf.setAttr('cpuRequests', params.CPU_REQUEST)
         this.conf.setAttr('memoryRequests', params.MEMORY_REQUEST)
