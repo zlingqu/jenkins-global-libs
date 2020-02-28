28085a2f6721b98903ce393be99ad8dc6d8d555c
左胜 <zuosheng@dm-ai.cn>
2020-02-28 16:06:31 +0800

    测试和开发环境支持自动生成的https域名

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -198,7 +198,7 @@ class JenkinsRunTemplate {
 
         // domain https
         if (this.conf.getAttr('https')) {
-            if (this.conf.getAttr('deployEnv') != 'prd') {
+            if (!(this.conf.getAttr('deployEnv') in ['prd', 'dev', 'test'])) {
                 this.conf.setAttr('domain', this.conf.getAttr('jobName') + "-" + this.conf.getAttr('namespace') + "-" +  this.conf.getAttr('deployEnv') + '.dm-ai.cn')
             }
         }
@@ -396,7 +396,7 @@ spec:
     private String templateDockersize() {
         return String.format('''
   - name: dockerize
-    image: docker.dm-ai.cn/devops/service-deploy-template:0.26
+    image: docker.dm-ai.cn/devops/service-deploy-template:0.28
     imagePullPolicy: IfNotPresent
     env: #指定容器中的环境变量
     - name: DMAI_PRIVATE_DOCKER_REGISTRY
