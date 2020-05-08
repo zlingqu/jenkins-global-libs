e88a98daf46224367fe745abb1d6d84cc40d8578
左胜 <zuosheng@dm-ai.cn>
2020-05-08 17:48:07 +0800

    等待时间设置为3秒

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -669,7 +669,7 @@ def call(Map map, env) {
                     container('kubectl') {
                         script {
                             sh "echo '检查部署在k8s集群中的服务的pod是否正常运行，等待限时1200秒。'"
-                            sh "sleep 20"
+                            sh "sleep 3"
                             try {
                                 kubernetesStatusCheck.waitKubernetesServerStartedV1()
                             } catch (e) {
