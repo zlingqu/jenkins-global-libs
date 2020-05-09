dc14574b872e10141438161c689fd96978e4df66
左胜 <zuosheng@dm-ai.cn>
2020-05-09 11:11:27 +0800

    使用特殊的编译参数和编译镜像

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -220,6 +220,9 @@ class JenkinsRunTemplate {
         this.conf.setAttr('if_add_unity_project', false)
         this.conf.setAttr('unity_app_name', 'no_unity')
         this.conf.setAttr('ifUseRootDockerfile', false)
+        this.conf.setAttr('ifCompileParam', false)
+        this.conf.setAttr('ifCompileImage', false)
+        this.conf.setAttr('compileImage', '')
         if (params.GLOABL_STRING != '') {
             def tmpStringList = params.GLOABL_STRING.split(":::")
             tmpStringList.length >= 1 ? this.conf.setAttr('useGrpc', tmpStringList[0]) : this.conf.setAttr('useGrpc', false)
@@ -228,6 +231,9 @@ class JenkinsRunTemplate {
             tmpStringList.length >= 4 ? this.conf.setAttr('if_add_unity_project', tmpStringList[3]) : this.conf.setAttr('if_add_unity_project', false)
             tmpStringList.length >= 5 ? this.conf.setAttr('unity_app_name', tmpStringList[4]) : this.conf.setAttr('unity_app_name', 'no_unity')
             tmpStringList.length >= 6 ? this.conf.setAttr('ifUseRootDockerfile', Boolean.parseBoolean(tmpStringList[5])) : this.conf.setAttr('ifUseRootDockerfile', false)
+            tmpStringList.length >= 7 ? this.conf.setAttr('ifCompileParam', Boolean.parseBoolean(tmpStringList[6])) : this.conf.setAttr('ifCompileParam', false)
+            tmpStringList.length >= 8 ? this.conf.setAttr('ifCompileImage', Boolean.parseBoolean(tmpStringList[7])) : this.conf.setAttr('ifCompileImage', false)
+            tmpStringList.length >= 9 ? this.conf.setAttr('compileImage', Boolean.parseBoolean(tmpStringList[8])) : this.conf.setAttr('compileImage', false)
         }
 
     }
