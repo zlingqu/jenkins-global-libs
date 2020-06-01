db41f824ebbc2f0f52f233ab951a201c460830cc
左胜 <zuosheng@dm-ai.cn>
2020-06-01 20:03:57 +0800

    test

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -445,7 +445,7 @@ spec:
     private String templateDockersize() {
         return String.format('''
   - name: dockerize
-    image: docker.dm-ai.cn/devops/service-deploy-template:0.59
+    image: docker.dm-ai.cn/devops/service-deploy-template:0.60
     imagePullPolicy: IfNotPresent
     env: #指定容器中的环境变量
     - name: DMAI_PRIVATE_DOCKER_REGISTRY
