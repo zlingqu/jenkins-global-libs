c3d16cc05ad8235649094635528562a19d935c59
liaolonglong <liaolonglong@dm-ai.cn>
2020-08-14 09:56:40 +0800

    测试新镜像

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -391,7 +391,6 @@ spec:
         return String.format('''
   - name: adp
     image: docker.dm-ai.cn/devops/base-image-adp:0.1.0
-    imagePullPolicy: IfNotPresent
     env:
     - name: VUE_APP_SCENE
       value: %s
