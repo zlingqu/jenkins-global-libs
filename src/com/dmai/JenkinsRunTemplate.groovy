811531dfcb29fd912118cf652ae4fad73479f8e7
dev-admin <dev-admin@dm-ai.cn>
2020-09-25 16:00:47 +0800

    Update JenkinsRunTemplate.groovy

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -390,7 +390,7 @@ spec:
     private String templateADP() {
         return String.format('''
   - name: adp
-    image: docker.dm-ai.cn/devops/base-image-adp:0.1.7%s
+    image: docker.dm-ai.cn/devops/base-image-adp:0.1.8%s
     imagePullPolicy: IfNotPresent
     env:
     - name: VUE_APP_SCENE
