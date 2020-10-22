859ce2cbab67d17c0f759a8e02046adf8ea431be
liaolonglong <liaolonglong@dm-ai.cn>
2020-10-22 14:02:57 +0800

    打印状态检查地址

--- src/com/dmai/KubernetesStatusCheck.groovy
+++ src/com/dmai/KubernetesStatusCheck.groovy
@@ -74,7 +74,7 @@ class KubernetesStatusCheck {
 
     private Map getServiceAppStatusV1(String imageSha) {
         def url0 = this.getServiceAppStatusV1Url(imageSha)
-        this.script.sh String.format('''echo 状态检查地址：%s end''',url0)
+        this.script.sh "echo '状态检查地址：${url0}'"
         URL url = new URL(url0)
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection()
