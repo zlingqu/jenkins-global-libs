08168e2993bf6ae6d98c25a90a10b437c5e2871e
quzhongling <zlingqu@126.com>
2020-12-01 10:18:21 +0800

    增加一个方法

--- src/com/tool/Tools.groovy
+++ src/com/tool/Tools.groovy
@@ -32,18 +32,31 @@ class Tools {
     }
 
     public static def addItemToListHead(list, item) {
-        def returnList = []
+        // def returnList = []
         if (item in list) {
-            returnList = list - item
-        } else {
-            returnList = list
+            // returnList = list - item
+            return list
         }
-        returnList = [item] + returnList
-        return returnList
+        // returnList = list
+        return [item] + list
+
+    // returnList = [item] + returnList
+    // return returnList
     }
 
     public static def handleBranchName(branchName) {
         return branchName.replaceAll('/', '-').replaceAll('_', '-').replaceAll('#', '-').replaceAll('@', '-').replaceAll('%', '-').replaceAll('[\\u4E00-\\u9FA5| ]+', '').toLowerCase()
+    }
+
+    //格式化输出
+    public static def PrintMes(value, color) {
+        def colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m",
+              'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
+              'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+        ansiColor('xterm') {
+            println(colors[color])
+        }
     }
 
 }
