85d9eeab91b0407091cb8aafc9a9c7004890e205
quzhongling <zlingqu@126.com>
2020-12-18 17:45:31 +0800

    修改内容

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -620,7 +620,7 @@ def call(Map map, env) {
                                 // adp 自动生成模板
                                 try {
                                     withEnv(conf.withEnvList) {
-                                    sh 'cd /workspace; dockerize -template src_dir:dest_dir  && cp -rp dest_dir/template.tmpl ./ && cat template.tmpl && chmod 777 template.tmpl'
+                                    sh 'cd /workspace; dockerize -template src_dir:dest_dir  && cp -rp dest_dir/template.tmpl ./ && cat template.tmpl && pwd && ls'
                                     }
                                 } catch (e) {
                                     sh "echo ${e}"
@@ -666,7 +666,7 @@ def call(Map map, env) {
                                     }
 
                                     deploykubernetes.deleteOldIngress()
-                                    sh 'kubectl apply -f template.tmpl'
+                                    sh 'pwd && ls && kubectl apply -f template.tmpl'
                                 }
 
                                 isCheckService = true
