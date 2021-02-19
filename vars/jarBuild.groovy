37f4b0fb93e78717589206eb99512db34ccc3c13
quzhongling <zlingqu@126.com>
2021-02-19 10:30:14 +0800

    删除无用的代码

--- vars/jarBuild.groovy
+++ vars/jarBuild.groovy
@@ -18,53 +18,32 @@ def call(Map map, env) {
     // 注入jenkins的环境变量到全局的Conf
     conf.setJenkinsAttrToConf(env, currentBuild)
 
-    // def branchName = conf.getAttr('branchName') ? conf.getAttr('branchName') : ''
-    // def deployEnv = Tools.addItemToListHead(['prd', 'dev', 'test', 'stage', 'jenkins', 'mlcloud-dev', 'lexue', 'tuoke', 'not-deploy'], conf.getDeployEnv())
-
-    println('【开始进行构建】')
+    println('【开始执行任务】')
     pipeline {
-        // parameters {
-        //     string(name: 'BRANCH_NAME', defaultValue: branchName, description: '代码分支名')
-        //     string(name: 'DEPLOY_MASTER_PASSWORD', defaultValue: 'please-input-password', description: '部署master分支请找运维人员输入密码自动部署')
-        //     choice(name: 'DEPLOY_ENV', choices: deployEnv, description: '部署的环境，目前支持：prd/dev/test/stage等')
-        // }
-
-
-        // environment {
-        //     gitVersion = "${params.GIT_VERSION}"
-        //     deployMasterPassword = "${params.DEPLOY_MASTER_PASSWORD}"
-        // }
-
         agent {
             kubernetes {
                 yaml new JenkinsRunTemplate(conf).getJenkinsRunTemplateOnJar()
                 cloud 'kubernetes-dev'
-                // label Tools.handleBranchName(conf.getAttr('branchName'))
                 label 'jar-build'
                 defaultContainer 'jnlp'
                 namespace 'devops'
                 inheritFrom 'base-template'
             }
         }
-
-        
         options {
             timestamps() //日志会显示时间
             timeout(time: 1, unit: 'HOURS') // 设置任务的超时时间为1个小时。
             buildDiscarder(logRotator(numToKeepStr: '200')) //保留最近200个构建
         }
 
-
         stages {
             stage('编译并上传jar包到nexue') {
-
                 steps {
                     container('compile') {
                         script {
                             sh 'pwd && ls -lh'
                             sh 'mvn -Dmaven.test.skip=true deploy'
                             sh 'pwd && ls -lh'
-                            
                         }
                     }
                 }
