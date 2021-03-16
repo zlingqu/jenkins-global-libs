afa57a1e96a7dabb997c66edab3d5b2348cb7d30
quzhongling <zlingqu@126.com>
2021-03-16 18:32:56 +0800

    更新基础镜像版本

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -366,7 +366,7 @@ spec:
     return String.format('''
   - name: adp
     imagePullPolicy: IfNotPresent
-    image: docker.dm-ai.cn/devops/base-image-adp:0.5.12%s
+    image: docker.dm-ai.cn/devops/base-image-adp:0.5.13%s
     env:
     - name: VUE_APP_SCENE
       value: %s
