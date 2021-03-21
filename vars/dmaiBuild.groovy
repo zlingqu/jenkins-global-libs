7df0ebb29fd42a32e552777736d7391a0e6aef4b
quzhognling <quzhongling>
2021-03-21 16:30:34 +0800

    debug

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -433,7 +433,7 @@ def call(Map map, env) {
                         when {
                             allOf {
                                 expression { return conf.getAttr('useModel') }
-                                expression { return conf.getAttr('ifUseGitManagerModel') }
+                                expression { return conf.getAttr('ifUseGitManagerModel') == true}
                             }
                         }
 
@@ -454,7 +454,31 @@ def call(Map map, env) {
                             }
                         }
                     }
+                    stage('使用NFS管理模型') {
+                        when {
+                            allOf {
+                                expression { return conf.getAttr('useModel') }
+                                expression { return !conf.getAttr('ifUseGitManagerModel') == false }
+                            }
+                        }
 
+                        steps {
+                            container('adp') {
+                                script {
+                                    try {
+                                        withCredentials([usernamePassword(credentialsId: 'devops-use', passwordVariable: 'password', usernameVariable: 'username')]) {
+                                            sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
+                                        }
+                                        sh 'pwd;ls -l;rm -fr model/.git'
+                                    } catch (e) {
+                                        sh "echo ${e}"
+                                        conf.failMsg = '从gitlab下载模型文件失败！'
+                                        throw e
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
