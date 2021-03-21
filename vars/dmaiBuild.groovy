0a1060aa61002fc5454e60ac86fc630842f331fa
quzhognling <quzhongling>
2021-03-21 16:16:09 +0800

    debug

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -468,7 +468,7 @@ def call(Map map, env) {
                                     try {
                                         // this.script.sh "mkdir -p ${conf.getAttr('modelPath')}; cp -rp /models/* ${conf.getAttr('modelPath')}"
                                         // this.script.sh "mkdir -p abc"
-                                        sh String.format('mkdir -p $s; cp -rp /models/* $s',conf.getAttr('modelPath'),conf.getAttr('modelPath'))
+                                        sh String.format('mkdir -p %s; cp -rp /models/* %s',conf.getAttr('modelPath'),conf.getAttr('modelPath'))
                                     } catch (e) {
                                         sh "echo ${e}"
                                         conf.failMsg = '存储中找不到模型文件，请先上传http://models.jenkins.dm-ai.cn/'
