8bd60cde787ea6ca5c7fa7c3b66a1f9d1d1cd93b
quzhongling <zlingqu@126.com>
2021-03-22 10:49:31 +0800

    debug

--- src/com/dmai/ModelManage.groovy
+++ src/com/dmai/ModelManage.groovy
@@ -13,20 +13,20 @@ class ModelManage {
     public void modelGitManage() {
         try {
             withCredentials([usernamePassword(credentialsId: 'devops-use', passwordVariable: 'password', usernameVariable: 'username')]) {
-                sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
+                this.script.sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
             }
-            sh 'pwd;ls -l;rm -fr model/.git'
+            this.script.sh 'pwd;ls -l;rm -fr model/.git'
         } catch (e) {
-            sh "echo ${e}"
+            this.script.sh "echo ${e}"
             conf.failMsg = '从gitlab下载模型文件失败！'
             throw e
         }
     }
     public void modelNfsManage() {
         try {
-            sh "mkdir -p ${conf.getAttr('modelPath')}; cp -rp /models/* ${conf.getAttr('modelPath')}"
+            this.script.sh "mkdir -p ${conf.getAttr('modelPath')}; cp -rp /models/* ${conf.getAttr('modelPath')}"
         } catch (e) {
-            sh "echo ${e}"
+            this.script.sh "echo ${e}"
             conf.failMsg = '从gitlab下载模型文件失败！'
             throw e
         }
