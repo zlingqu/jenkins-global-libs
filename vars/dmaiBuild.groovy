c6a459901da67fdb769afe59d69f69fdc310e89e
quzhongling <zlingqu@126.com>
2021-03-22 11:22:06 +0800

    1debug1

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -447,7 +447,10 @@ def call(Map map, env) {
                         steps {
                             container('adp') {
                                 script {
-                                    modelManage.modelGitManage()
+                                    // modelManage.modelGitManage()
+                                    withCredentials([usernamePassword(credentialsId: 'devops-use', passwordVariable: 'password', usernameVariable: 'username')]) {
+                this.script.sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + this.conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
+            }
                                 }
                             }
                         }
