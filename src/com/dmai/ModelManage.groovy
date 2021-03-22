af9e83d1f38f76168326df85b1ec574f8aabcdd9
quzhongling <zlingqu@126.com>
2021-03-22 11:07:58 +0800

    debug1

--- src/com/dmai/ModelManage.groovy
+++ src/com/dmai/ModelManage.groovy
@@ -16,16 +16,16 @@ class ModelManage {
         this.script.sh "pwd"
     }
     public void modelGitManage() {
-        try {
+        // try {
             withCredentials([usernamePassword(credentialsId: 'devops-use', passwordVariable: 'password', usernameVariable: 'username')]) {
                 this.script.sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + this.conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
             }
             this.script.sh "pwd;ls -l;rm -fr model/.git"
-        } catch (e) {
-            this.script.sh "echo ${e}"
-            this.conf.failMsg = '从gitlab下载模型文件失败！'
-            throw e
-        }
+        // } catch (e) {
+        //     this.script.sh "echo ${e}"
+        //     this.conf.failMsg = '从gitlab下载模型文件失败！'
+        //     throw e
+        // }
     }
     // public void modelNfsManage() {
     //     try {
