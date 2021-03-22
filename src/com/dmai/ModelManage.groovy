bb417c7de826c661df4be418a2a278bf3fe37d92
quzhongling <zlingqu@126.com>
2021-03-22 11:05:49 +0800

    debug1

--- src/com/dmai/ModelManage.groovy
+++ src/com/dmai/ModelManage.groovy
@@ -9,24 +9,24 @@ class ModelManage {
         this.script = script
         this.conf = conf
     }
-    public void modelGitManage() {
-        this.script.sh "pwd"
-    }
+    // public void modelGitManage() {
+    //     this.script.sh "pwd"
+    // }
     public void modelNfsManage() {
         this.script.sh "pwd"
     }
-    // public void modelGitManage() {
-    //     try {
-    //         withCredentials([usernamePassword(credentialsId: 'devops-use', passwordVariable: 'password', usernameVariable: 'username')]) {
-    //             this.script.sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + this.conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
-    //         }
-    //         this.script.sh 'pwd;ls -l;rm -fr model/.git'
-    //     } catch (e) {
-    //         this.script.sh "echo ${e}"
-    //         this.conf.failMsg = '从gitlab下载模型文件失败！'
-    //         throw e
-    //     }
-    // }
+    public void modelGitManage() {
+        try {
+            withCredentials([usernamePassword(credentialsId: 'devops-use', passwordVariable: 'password', usernameVariable: 'username')]) {
+                this.script.sh 'source /etc/profile; git config --global http.sslVerify false ; git clone ' + this.conf.getAttr("modelGitRepository").replace("https://", 'https://$username:$password@') + ' model'
+            }
+            this.script.sh "pwd;ls -l;rm -fr model/.git"
+        } catch (e) {
+            this.script.sh "echo ${e}"
+            this.conf.failMsg = '从gitlab下载模型文件失败！'
+            throw e
+        }
+    }
     // public void modelNfsManage() {
     //     try {
     //         this.script.sh String.format("mkdir -p %s; cp -rp /models/* %s",this.conf.getAttr('modelPath'),this.conf.getAttr('modelPath'))
