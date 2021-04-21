56de096f13811b98ef30151e665314b189787a20
quzhongling <zlingqu@126.com>
2021-04-21 10:53:40 +0800

    debug

--- src/com/dmai/Compile.groovy
+++ src/com/dmai/Compile.groovy
@@ -26,7 +26,7 @@ class Compile {
     public void compileOfJs() {
         def tmpJsCompileString = 'npm config set registry https://npm.dm-ai.cn/repository/npm/ && npm install && npm run build || echo'
 
-        this.script.sh String.format("ln -sv /data/cache/node_modules node_modules"+"export FRONTEND_ENV=%s; %s", this.conf.getAttr('nodeEnv'), tmpJsCompileString)
+        this.script.sh String.format("ln -sv /data/cache/node_modules node_modules;"+"export FRONTEND_ENV=%s; %s", this.conf.getAttr('nodeEnv'), tmpJsCompileString)
         // this.script.sh String.format("test -e node_modules && rm -fr node_modules ; " +
         //         "test -e /data/cache/node_modules/node_modules.tar && cp -rp /data/cache/node_modules/node_modules.tar ./ ; tar xf node_modules.tar && rm -fr node_modules.tar ; " +
         //         "export FRONTEND_ENV=%s; %s && tar cf node_modules.tar node_modules;" +
