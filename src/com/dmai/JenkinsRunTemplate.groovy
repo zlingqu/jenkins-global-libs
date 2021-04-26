e7ebf06fbcfd70c73e65900db6badee75d316c83
quzhongling <zlingqu@126.com>
2021-04-26 10:44:59 +0800

    更改基础镜像版本

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -459,7 +459,7 @@ spec:
             case ['js','node','nodets']:
         return String.format('''
   - name: compile
-    image: docker.dm-ai.cn/public/node:10.15.3-slim-base-0.1
+    image: docker.dm-ai.cn/public/node:10.15.3-slim-base-0.2
     imagePullPolicy: IfNotPresent
     securityContext:
       capabilities:
