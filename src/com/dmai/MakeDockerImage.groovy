3c0fa381d1c93c10fd7078eab60f0cec7dc47a5b
quzhongling <zlingqu@126.com>
2021-04-29 18:17:26 +0800

    debug

--- src/com/dmai/MakeDockerImage.groovy
+++ src/com/dmai/MakeDockerImage.groovy
@@ -27,7 +27,7 @@ class MakeDockerImage {
             this.script.sh "echo '${this.dockerFileTemplate.getDockerFile()}' > Dockerfile"
         }
 
-        if (conf.getAttr('customDockerfile') && !this.conf.getAttr('ifUseRootDockerfile') {
+        if (conf.getAttr('customDockerfile') && !this.conf.getAttr('ifUseRootDockerfile')) {
             this.script.sh "echo '${this.conf.getAttr('customDockerfileContent')}' > Dockerfile"
         }
 
