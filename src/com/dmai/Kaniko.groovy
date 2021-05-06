1d6b4a87e78baac317bd4aee366be1ae06099cd5
quzhongling <zlingqu@126.com>
2021-05-06 15:39:55 +0800

    debug

--- src/com/dmai/Kaniko.groovy
+++ src/com/dmai/Kaniko.groovy
@@ -14,10 +14,6 @@ class Kaniko {
     }
     public void makeAndPushImage() {
         this.script.sh 'echo quzl'
-        this.script.sh 'executor --destination $IMAGE_TAG_NAME' + this.conf.getAttr('buildImageAddress')
-//         this.script.sh """\
-// #!/busybox/sh -e
-// echo quzl
-// """
+        this.script.sh 'executor --destination' + this.conf.getAttr('buildImageAddress')
     }
 }
