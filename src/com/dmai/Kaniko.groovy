650636dcb9529a17000209d91965aa5457f4be22
quzhongling <zlingqu@126.com>
2021-05-07 14:43:43 +0800

    debug

--- src/com/dmai/Kaniko.groovy
+++ src/com/dmai/Kaniko.groovy
@@ -14,11 +14,11 @@ class Kaniko {
     }
     public void makeAndPushImage() {
         this.script.sh 'echo quzl'
-        this.script.sh """ pwd && tree -L 2 && \
+        this.script.sh "pwd && tree -L 2 && \
         executor --context ${WORKSPACE} \
         --build-arg MODEL_VERSION=${this.conf.modelVersion} \
         --build-arg FRONTEND_ENV=${this.conf.modelVersion} \
         --build-arg VUE_APP_SCENE=${this.conf.getAttr('nodeEnv')} \
-        --destination ${this.conf.getAttr('buildImageAddress')} """
+        --destination ${this.conf.getAttr('buildImageAddress')}"
     }
 }
