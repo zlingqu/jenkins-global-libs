2696c976c4aa4d089cf1351a024f1221a1df1a5f
quzhongling <zlingqu@126.com>
2021-05-07 15:00:37 +0800

    debug

--- src/com/dmai/Kaniko.groovy
+++ src/com/dmai/Kaniko.groovy
@@ -19,11 +19,11 @@ class Kaniko {
         // this.conf.getAttr('nodeEnv'),
         // this.conf.vueAppScene,
         // this.conf.getAttr('buildImageAddress'))
-        this.script.sh 'pwd && tree -L 2 && executor --context ' +
-        '${WORKSPACE}' +
-        '--build-arg MODEL_VERSION=${this.conf.modelVersion}' +
-        '--build-arg FRONTEND_ENV=${this.conf.modelVersion}' +
-        '--build-arg VUE_APP_SCENE=${this.conf.getAttr('nodeEnv')' +
-        '--destination ${this.conf.getAttr('buildImageAddress')}'
+        this.script.sh "pwd && tree -L 2 && executor --context " +
+        "${WORKSPACE}" +
+        "--build-arg MODEL_VERSION=${this.conf.modelVersion}" +
+        "--build-arg FRONTEND_ENV=${this.conf.modelVersion}" +
+        "--build-arg VUE_APP_SCENE=${this.conf.getAttr('nodeEnv')}" +
+        "--destination ${this.conf.getAttr('buildImageAddress')}"
     }
 }
