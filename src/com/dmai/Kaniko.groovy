5c104ae9b122eabaf85a6b72b6e3e761494e32d3
quzhongling <zlingqu@126.com>
2021-05-07 14:51:53 +0800

    debug

--- src/com/dmai/Kaniko.groovy
+++ src/com/dmai/Kaniko.groovy
@@ -14,8 +14,7 @@ class Kaniko {
     }
     public void makeAndPushImage() {
         this.script.sh 'echo quzl'
-        this.script.sh String.format('pwd && tree -L 2 && executor --context %s --build-arg MODEL_VERSION=%s --build-arg FRONTEND_ENV=%s --build-arg VUE_APP_SCENE=%s --destination %s',
-        WORKSPACE,
+        this.script.sh String.format('pwd && tree -L 2 && executor --context ${WORKSPACE} --build-arg MODEL_VERSION=%s --build-arg FRONTEND_ENV=%s --build-arg VUE_APP_SCENE=%s --destination %s',
         this.conf.modelVersion,
         this.conf.getAttr('nodeEnv'),
         this.conf.vueAppScene,
