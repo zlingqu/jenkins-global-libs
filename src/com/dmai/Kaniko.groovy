e1d03628bebd3e783bc060226880b4abe45d1573
quzhongling <zlingqu@126.com>
2021-05-07 14:49:32 +0800

    debug

--- src/com/dmai/Kaniko.groovy
+++ src/com/dmai/Kaniko.groovy
@@ -14,11 +14,17 @@ class Kaniko {
     }
     public void makeAndPushImage() {
         this.script.sh 'echo quzl'
-        this.script.sh "pwd && tree -L 2 && \
-        executor --context ${WORKSPACE} \
-        --build-arg MODEL_VERSION=${this.conf.modelVersion} \
-        --build-arg FRONTEND_ENV=${this.conf.modelVersion} \
-        --build-arg VUE_APP_SCENE=${this.conf.getAttr('nodeEnv')} \
-        --destination ${this.conf.getAttr('buildImageAddress')}"
+        this.script.sh String.format('pwd && tree -L 2 && executor --context %s --build-arg MODEL_VERSION=%s --build-arg FRONTEND_ENV=%s --build-arg VUE_APP_SCENE=%s --destination %s',
+        WORKSPACE,
+        this.conf.modelVersion,
+        this.conf.getAttr('nodeEnv'),
+        this.conf.vueAppScene,
+        this.conf.getAttr('buildImageAddress'))
+    //     this.script.sh "pwd && tree -L 2 && \
+    //     executor --context ${WORKSPACE} \
+    //     --build-arg MODEL_VERSION=${this.conf.modelVersion} \
+    //     --build-arg FRONTEND_ENV=${this.conf.modelVersion} \
+    //     --build-arg VUE_APP_SCENE=${this.conf.getAttr('nodeEnv')} \
+    //     --destination ${this.conf.getAttr('buildImageAddress')}"
     }
 }
