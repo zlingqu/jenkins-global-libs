8acb6af48bf9e78d61ea94070afad5ea92c95341
quzhongling <zlingqu@126.com>
2021-05-08 18:10:16 +0800

    debug

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -330,7 +330,7 @@ spec:
     return String.format('''
   - name: adp
     imagePullPolicy: IfNotPresent
-    image: docker.dm-ai.cn/devops/base-image-adp:0.5.34%s
+    image: docker.dm-ai.cn/devops/base-image-adp:0.5.35%s
     env:
     - name: VUE_APP_SCENE
       value: %s
@@ -352,7 +352,7 @@ spec:
     return String.format('''
   - name: kaniko
     imagePullPolicy: IfNotPresent
-    image: docker.dm-ai.cn/devops/base-image-adp:0.5.34
+    image: docker.dm-ai.cn/devops/base-image-adp:0.5.35
     command:
     - "sleep"
     args:
