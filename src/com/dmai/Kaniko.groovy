be82fee0b17402127946efbb2b0f51d2bd91fd92
quzhongling <zlingqu@126.com>
2021-05-10 10:14:52 +0800

    debug

--- src/com/dmai/Kaniko.groovy
+++ src/com/dmai/Kaniko.groovy
@@ -14,26 +14,19 @@ class Kaniko {
     }
     public void makeAndPushImage() {
         // this.script.sh 'echo quzl'
-        this.script.sh echo ' echo "{\"auths\":{\"$DOCKER_REGIS_URL\":{\"username\":\"$DOCKER_LIB_USER\",\"password\":\"$DOCKER_LIB_PASSWORD\"}}}" > /kaniko/.docker/config.json'
+        // this.script.sh echo ' echo "{\"auths\":{\"$DOCKER_REGIS_URL\":{\"username\":\"$DOCKER_LIB_USER\",\"password\":\"$DOCKER_LIB_PASSWORD\"}}}" > /kaniko/.docker/config.json'
+        // this.script.sh String.format('pwd && tree -L 2 && /kaniko/executor --verbosity error --context ${WORKSPACE} --dockerfile ${WORKSPACE}/Dockerfile --build-arg MODEL_VERSION=%s --build-arg FRONTEND_ENV=%s --build-arg VUE_APP_SCENE=%s --destination %s',
+        // this.conf.modelVersion,
+        // this.conf.getAttr('nodeEnv'),
+        // this.conf.vueAppScene,
+        // this.conf.getAttr('buildImageAddress'))
+
+        // this.script.sh echo ' echo "{\"auths\":{\"$DOCKER_REGIS_URL\":{\"username\":\"$DOCKER_LIB_USER\",\"password\":\"$DOCKER_LIB_PASSWORD\"}}}" > /kaniko/.docker/config.json'
         this.script.sh String.format('pwd && tree -L 2 && /kaniko/executor --verbosity error --context ${WORKSPACE} --dockerfile ${WORKSPACE}/Dockerfile --build-arg MODEL_VERSION=%s --build-arg FRONTEND_ENV=%s --build-arg VUE_APP_SCENE=%s --destination %s',
         this.conf.modelVersion,
         this.conf.getAttr('nodeEnv'),
         this.conf.vueAppScene,
         this.conf.getAttr('buildImageAddress'))
 
-
-        // this.script.sh String.format('cp -r * /workspace && cd /workspace && pwd && tree -L 2 && /kaniko/executor --context /workspace --dockerfile /workspace/Dockerfile --build-arg MODEL_VERSION=%s --build-arg FRONTEND_ENV=%s --build-arg VUE_APP_SCENE=%s --destination %s',
-        // this.conf.modelVersion,
-        // this.conf.getAttr('nodeEnv'),
-        // this.conf.vueAppScene,
-        // this.conf.getAttr('buildImageAddress'))
-
-
-        // this.script.sh "pwd && tree -L 2 && executor --context " +
-        // "${WORKSPACE}" +
-        // "--build-arg MODEL_VERSION=${this.conf.modelVersion}" +
-        // "--build-arg FRONTEND_ENV=${this.conf.modelVersion}" +
-        // "--build-arg VUE_APP_SCENE=${this.conf.getAttr('nodeEnv')}" +
-        // "--destination ${this.conf.getAttr('buildImageAddress')}"
     }
 }
