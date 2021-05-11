865ce590588cac993d92f07c308efb3425286855
quzhongling <zlingqu@126.com>
2021-05-11 19:10:07 +0800

    添加参数

--- src/com/dmai/JenkinsRunTemplate.groovy
+++ src/com/dmai/JenkinsRunTemplate.groovy
@@ -336,7 +336,7 @@ spec:
     return String.format('''
   - name: adp
     imagePullPolicy: IfNotPresent
-    image: docker.dm-ai.cn/devops/base-image-adp:0.5.47%s
+    image: docker.dm-ai.cn/devops/base-image-adp:0.5.48%s
     env:
     - name: VUE_APP_SCENE
       value: %s
