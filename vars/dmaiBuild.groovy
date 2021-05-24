8899538fd9f03784ae9e47b6c571063f9571fbfc
quzhongling <zlingqu@126.com>
2021-05-24 11:36:15 +0800

    debug

--- vars/dmaiBuild.groovy
+++ vars/dmaiBuild.groovy
@@ -733,6 +733,7 @@ def call(Map map, env) {
                         steps {
                             container('adp') {
                                 script {
+                                    sh 'echo docker plugin quzhognling'
                                     docker.withRegistry(conf.getAttr('buildImageAddress'), conf.getAttr('buildImageAddress')) {
                                     // docker.withRegistry('https://docker.dm-ai.cn', 'docker.dm-ai.cn') {
                                         def customImage = docker.build("my-image:${env.BUILD_ID}")
